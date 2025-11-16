f35d2f87302c5eb6e8db882b95304bfc6acfcb09
Prashant Shukla <mrprashantshukla149@gmail.com>
2025-11-16 18:44:08 +0530

    Time: 4 ms (90.05%) | Memory: 46.2 MB (14.66%) - LeetSync

---
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
@@ -0,0 +1,19 @@
+class Solution {
+    static final int M = 1_000_000_007;
+    public int numSub(String s) {
+        long result =0;
+        long count1 = 0;
+
+        for (char ch : s.toCharArray()){
+            if(ch == '1'){
+                count1++;
+                result = (result + count1)%M;
+            }else{
+                count1 = 0;
+            }
+        }
+        return (int)result;
+
+        
+    }
+}
