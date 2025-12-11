369f527dec74d21388c1c8592671250ea554acf6
Prashant Shukla <mrprashantshukla149@gmail.com>
2025-12-12 01:51:39 +0530

    Time: 136 ms (62.89%) | Memory: 275 MB (34.02%) - LeetSync

---
+++ 3819-count-covered-buildings/count-covered-buildings.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int countCoveredBuildings(int n, int[][] buildings) {
+        Map<Integer, int[]> yToMinMaxX = new HashMap<>();
+        Map<Integer, int[]> xToMinMaxY = new HashMap<>();
+
+        for (int[] building : buildings) {
+            int x = building[0];
+            int y = building[1];
+
+            yToMinMaxX.putIfAbsent(y, new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE});
+            xToMinMaxY.putIfAbsent(x, new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE});
+
+            int[] xr = yToMinMaxX.get(y);
+            xr[0] = Math.min(xr[0], x);
+            xr[1] = Math.max(xr[1], x);
+
+            int[] yr = xToMinMaxY.get(x);
+            yr[0] = Math.min(yr[0], y);
+            yr[1] = Math.max(yr[1], y);
+        }
+
+        int result = 0;
+
+        for (int[] building : buildings) {
+            int x = building[0];
+            int y = building[1];
+
+            int[] xr = yToMinMaxX.get(y);
+            int[] yr = xToMinMaxY.get(x);
+
+            if (xr[0] < x && x < xr[1] &&
+                yr[0] < y && y < yr[1]) {
+                result++;
+            }
+        }
+
+        return result;
+        
+    }
+}
